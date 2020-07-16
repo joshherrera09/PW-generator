fea8f648e2324f435dd134ca7bade4ae1ddcbc6e
Josh Herrera <joshherrera09@gmail.com>
2020-07-16 16:29:37 -0700

    added prompts to script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+// prompts to gather user info
+alert("Please help us generate your password. Choose between 8-128 characters in length, and choose at least one of the character types")
+var passLength = prompt("How many characters would you like your pasword to be? 8-128 characters");
+var lowercase = confirm("Would you like to use lower case letters?");
+var uppercase = confirm("Would you like to use upperacse letters?");
+var numeric = confirm("Would you like to use numbers?");
+var specialChar = confirm("Would you like to use special characters? ex: ! $ # _ etc.");
+
+
+function generate() {
+
+}
+
