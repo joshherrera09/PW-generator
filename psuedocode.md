e743c24c28f39cebbbd414c1d7ac377ba954978c
Josh Herrera <joshherrera09@gmail.com>
2020-07-21 01:18:11 -0700

    getRadnomInt/generatePassword functions

---
+++ psuedocode.md
@@ -0,0 +1,38 @@
+* HW # 3 psuedo code
+
+This program will generate a random password based on questions answered by the user.
+
+Determine length of password
+    'PLease enter a number 8-128 to determine the length of your password'
+
+Determine criteria that will be used to form password by sereies of confirms
+    'Do you want uppercase letters?'
+    'Do you want lowercase letters?'
+    'Do you want special characters?'
+    'Do you want numbers?'
+
+After the criteria is determined the user will hit the 'generate pasword' button and a random password 
+will be issued using the especific criteria determined
+
+If user chooses uppercase   
+    then include uppercase letters
+        else if user doesnt choose uppercase
+            then dont include uppercase letters
+If user chooses lowercase
+    then use lowercase letters  
+        else if user doesnt choose lowercase 
+            then dont include lowercase letters
+If user chooses special char   
+    then include special char letters
+        else if user doesnt choose special char
+            then dont include special char letters
+If user chooses numbers  
+    then include numbers 
+        else if user doesnt choose numbers
+            then dont include numbers
+
+Depending on the length the user decides, the generate password button will generate an array of letters, numbers and special characters
+
+The page will display the password in the box on the page
+
+
--- script.js
+++ script.js
@@ -1,45 +1,100 @@
-alert("Hello, please help us generate your password")
-var passLength = parseInt(prompt("Please enter a number 8-128, to determine the length of your password")
-var upperCaseLet = 
+alert("Hello, please help us generate your password");
+const passLength = parseInt(prompt("Please enter a number 8-128, to determine the length of your password"));
+
+let charTypes = [];
 
 
+
+let password = '';
+
+const lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+const upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+const specialCharacters = ['!', '$', '%', '&', '_', '?', '#', '@'];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function getPasswordRequirements() {
+ 
+  let useUpper = confirm('Do you want upper case letters?');
+  let useLower = confirm('Do you want lower case letters?');
+  let useSpecialChar = confirm('Do you want special characters?');
+  let useNumbers = confirm('Do you want numbers?');
 
+  if (useUpper === true) {
+    charTypes.push('upperCase');
+  }
+
+  if (useLower === true) {
+    charTypes.push('lowerCase');
+  }
+
+  if (useSpecialChar === true) {
+    charTypes.push('specialCharacters');
+  }
+
+  if (useNumbers === true) {
+    charTypes.push('numbers');
+  }
+}
 
 //generate password
 
-generateBtn.addEventListener("click", writepassword);
-
 function generatePassword() {
-    var lowerCaseLetters = ();
-    var upperCaseLetters = ();
-    var specialChar =();
+  let charType;
+  let index;
+  let newChar;
+  let pWord = '';
+  
+  for (var i = 0; i < passLength; i++) {
+    index = getRandomInt(charTypes.length);
+    charType = charTypes[index];
     
-    var passLength = 0;
-
-    var password = ""
-
-    for var i=0; i < passLength; i++) {
-        var xletter = Math.floor(Math.random() * lowerCaseLetters.length);
-        generatePassword += lowerCaseLetters.substring(xletter, xletter+1);
+    if (charType === 'lowerCase') {
+      index = getRandomInt(lowerCase.length);
+      newChar = lowerCase[index];
+      pWord = pWord + newChar;
     }
-    document.getElementsByTagName('textarea')[0].value = '#password'
+
+    if (charType === 'upperCase') {
+      index = getRandomInt(upperCase.length);
+      newChar = upperCase[index];
+      pWord = pWord + newChar;
+    }
+
+    if (charType === 'specialCharacters') {
+      index = getRandomInt(specialCharacters.length);
+      newChar = specialCharacters[index];
+      pWord = pWord + newChar;
+    }
+
+    if (charType === 'numbers') {
+      index = getRandomInt(numbers.length);
+      newChar = numbers[index];
+      pWord = pWord + newChar;
+    }
+  }
+  return pWord;
 
 }
 
+  // Write password to the #password input
+  function writePassword() {
+    charTypes = [];
+    getPasswordRequirements()
+    password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
+    passwordText.value = password;
+
+  }
+
+
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
 
